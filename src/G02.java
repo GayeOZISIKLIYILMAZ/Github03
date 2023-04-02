a125a553d1928b043e065bf749c42cc140544c2c
Gaye Yılmaz <gayeozisikli@gmail.com>
2023-04-02 16:42:28 +0200

    4.gün

---
+++ src/G02.java
@@ -0,0 +1,19 @@
+public class G02 {
+
+    /*
+    Git githubda projelerimizin üstünde çalışırke 3 adımda işlem yapıyoruz
+
+    1-Projelerimizi veya dosyalarımızı git işe ilişkilendiriyoruz -git init-
+    2-Projemizde veya dosyamızda bulunanları version oluşturuyoruz.
+         git add.
+         git commit -m"versiyonadı";
+     3-Githuba yolluyoruz
+
+        git remote add origin repoUrlAdersi(ilk kez yapıyorsak)
+        already origin (tanıtmışsak)
+        git remote set-url repoUrlAdresi
+
+        git push
+
+     */
+}
